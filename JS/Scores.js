4140a981c575b1a0c5e7e3e515cc0f629c84b5d0
Mcnoor <99060667+Mcnoor@users.noreply.github.com>
2022-04-03 23:49:28 -0500

    update

--- JS/Scores.js
+++ JS/Scores.js
@@ -2,7 +2,7 @@
 
 //Stored in localstorage after reset if needed
 function printTopScores() {
-  var topscores= JSON.parse(window.localStorage.getItem ("topscores")) || [];
+  var topscores= JSON.parse(window.localStorage.getItem ("highscores")) || [];
 
   // Score order highest to lowest
   topscores.sort(function (a, b) {
@@ -14,18 +14,18 @@ topscores.forEach(function(score) {
   var liTag = document.createElement("li");
   liTag.textContent = score.intials3 + " " + score.score;
 
-  var liTag=document.getElementById("topscores");
+  var liTag=document.getElementById("highscores");
   olElement.appendChild(liTag);
 });
 }
 
 function removeHighscores () {
-  window.localStorage.removeItem ("topscores");
+  window.localStorage.removeItem ("highscores");
   window.location.reload();
 }
 
 document.getElementById("remove").onclick = removeHighscores;
 
 // When page loads run funtions
-printTopScores();
+printHighscores();
 
--- JS/script.js
+++ JS/script.js
@@ -142,7 +142,7 @@ function saveHighscore() {
     window.localStorage.setItem("highscores", JSON.stringify(highscores));
 
     // redirect to next page
-    window.location.href = "score.html";
+    window.location.href = "topscore.html";
   }
 }
 
